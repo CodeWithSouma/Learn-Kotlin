b7ed04f07a4ba5a026e48b22aa2ffcf5e6a4ccc9
Souma <souma.hitech@gmail.com>
2020-12-25 00:43:51 +0530

    modify function code

--- src/Function.kt
+++ src/Function.kt
@@ -1,10 +1,18 @@
+// this function return sum of two number
 fun calculateSum(number1 : Int, number2 : Int) : Int{
     return number1 + number2
 }
+
+// this function only print sum of two number
+fun printSum(number1: Int, number2: Int): Unit{// we can exclude unit return type unit means this function return nothing
+    println("Sum of $number1 and $number2 : ${number1 + number2}") // here we use templating for format the string
+}
+
 
 fun main(){
     // val use for defining constant
     // val use for defining variable
    val result = calculateSum(10,20)
-    print("Result: $result");
+    println("Result: $result");
+    printSum(4,5)
 }
