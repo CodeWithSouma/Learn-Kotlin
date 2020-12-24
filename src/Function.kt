a174b05dafa2a2a63a0f6f7382c225ff15d7a6ef
Souma <souma.hitech@gmail.com>
2020-12-25 00:43:21 +0530

    complete function

---
+++ src/Function.kt
@@ -0,0 +1,10 @@
+fun calculateSum(number1 : Int, number2 : Int) : Int{
+    return number1 + number2
+}
+
+fun main(){
+    // val use for defining constant
+    // val use for defining variable
+   val result = calculateSum(10,20)
+    print("Result: $result");
+}
