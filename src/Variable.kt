fcc48970826e920608754c20c26ff5a2b03684d2
Souma <souma.hitech@gmail.com>
2020-12-25 12:53:14 +0530

    complete veriable declairation and initialization

---
+++ src/Variable.kt
@@ -0,0 +1,20 @@
+fun main(){
+    // var means this variable value can be change
+    // val means this is a constant we can't changed the value of this variable
+    //Here you can exclude datatype because whe you define variable
+    // that time you initialized the variable if you don't
+    // initialized the you need to specify the variable type  for example name variable
+    // you have to use datatype
+    var name :String
+    var x: Int = 5
+    val y: Int = 6
+
+    name = "Soumadip Dey"
+
+    x = 10
+    //y = 7// can't be reassigned
+
+    println("Name: $name")
+    println("X: $x")
+    println("X: $y")
+}
