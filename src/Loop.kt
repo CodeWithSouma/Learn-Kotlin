dc1795a7abc4f4fb7ed5206e780552ab68c38416
Souma <souma.hitech@gmail.com>
2020-12-25 22:54:19 +0530

    complete for each loop

--- src/Loop.kt
+++ src/Loop.kt
@@ -6,4 +6,7 @@ fun main() {
     for ( name in names)
         println(name)
 
+    // for loop
+    println("Print array element using for each loop")
+    names.forEach { name -> println(name) }
 }
