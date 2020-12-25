b33f4951f88df84b4d41735cd8e62a261d54ffa3
Souma <souma.hitech@gmail.com>
2020-12-25 22:53:18 +0530

    complete for loop

---
+++ src/Loop.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val names = arrayOf("Souma","Arijit","Soumik")
+
+    // for loop
+    println("Print array element using for loop")
+    for ( name in names)
+        println(name)
+
+}
