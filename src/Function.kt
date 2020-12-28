b4c86f1371a056ed095892a16f5b5c1bcea39c6e
Souma <souma.hitech@gmail.com>
2020-12-28 12:48:35 +0530

    complete higher order function

--- src/Function.kt
+++ src/Function.kt
@@ -8,11 +8,27 @@ fun printSum(number1: Int, number2: Int): Unit{// we can exclude unit return typ
     println("Sum of $number1 and $number2 : ${number1 + number2}") // here we use templating for format the string
 }
 
+// default value in function
+fun greet(greet:String = "Good Morning",name:String){
+    println("Hello!!! $name $greet")
+}
 
-fun main(){
+// higher order function
+fun calculator(number1: Int, number2: Int, operator: (Int, Int) -> Int):Int{
+ return operator(number1 , number2)
+}
+
+
+fun main() {
     // val use for defining constant
     // val use for defining variable
-   val result = calculateSum(10,20)
+    val result = calculateSum(10, 20)
     println("Result: $result");
-    printSum(4,5)
+    printSum(4, 5)
+    greet(name = "Souma")// no need to pass first argument
+    greet(name = "Soumaik", greet = "Good Evening")//when you pass named argument then you don't need to maintain sequence of parameter
+
+    // higher function in kotlin
+    val sum = calculator(5, 6, {a , b -> a + b} )
+    println(sum)
 }
